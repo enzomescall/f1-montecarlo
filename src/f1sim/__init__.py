477642068b65e468d38f75e76414bbabd8b3cd73
Enzo Mescall <emm121@duke.edu>
2025-09-02 22:22:58 -0400

    basic layout

---
+++ src/f1sim/__init__.py
@@ -0,0 +1,11 @@
+# src/f1sim/__init__.py
+"""
+f1sim: Monte Carlo Formula 1 race simulator.
+"""
+
+from .race import RaceSim
+from .track import Track
+from .car import DriverCar, CarState, TireSpec
+
+__all__ = ["RaceSim", "Track", "DriverCar", "CarState", "TireSpec"]
+__version__ = "0.0.1"
